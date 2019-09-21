1e7e6cadc16a3fb6ce400b40ca4bacba8741ccc5
daehwa-seo <daehwa.seo@dealersolutions.com.au>
2019-09-21 12:12:05 +1000

    Components (Header Nav, Landing Page, Search Form, Search Results) are created.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { SearchFormComponent } from './components/search-form/search-form.component';
+import { SearchResultsComponent } from './components/search-results/search-results.component';
+import { HeaderNavComponent } from './components/header-nav/header-nav.component';
+import { LandingPageComponent } from './components/landing-page/landing-page.component';
+
+const appRoutes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: LandingPageComponent },
+  { path: 'results', component: SearchResultsComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchFormComponent,
+    SearchResultsComponent,
+    HeaderNavComponent,
+    LandingPageComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
